9863535809ed2800e8295a542806ed2acfde506c
cshimm <cameronjshimmin@gmail.com>
2024-06-09 21:24:02 -0700

    fix uri

--- util/queries.js
+++ util/queries.js
@@ -1,6 +1,6 @@
 import {useQuery} from "@tanstack/react-query";
 
-const url = `${process.env.URI}/api`
+const url = `${process.env.EXPO_PUBLIC_API_URI}/api`
 const getBridges = async () => {
     const response = await fetch(`${url}/bridges`)
     return response.json();
