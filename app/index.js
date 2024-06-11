63009893ac08f2908d83d0cf8c403d0d6115e695
cshimm <cameronjshimmin@gmail.com>
2024-06-11 16:32:30 -0700

    optimize imports

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Navbar} from "../components/Navbar";
 import {Bridges} from "../components/Bridges";
