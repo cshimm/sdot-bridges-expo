f0957192051aa28b828a5d13a1bd21155b8dd924
cshimm <cameronjshimmin@gmail.com>
2024-06-11 12:22:08 -0700

    added favoriting

--- app/index.js
+++ app/index.js
@@ -18,8 +18,6 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: "center",
-    // padding: 24,
   },
   main: {
     flex: 1,
--- components/Bridge.jsx
+++ components/Bridge.jsx
@@ -3,17 +3,41 @@ import {formatDate} from "../util/dateUtils";
 import {FontAwesome} from "@expo/vector-icons";
 import {useEffect, useState} from "react";
 import {bridgeStyles as styles} from "../styles/bridgeStyles";
+import {useUserById} from "../util/queries";
+import {getCurrentUserId} from "../util/userUtils";
 
+const URL = process.env.EXPO_PUBLIC_API_URI;
 export const Bridge = ({bridge}) => {
     const [token, setToken] = useState();
     const [isFave, setIsFave] = useState(false)
-
+    const {data: user, refetch} = useUserById(getCurrentUserId());
     useEffect(() => {
         setToken(localStorage.getItem('token'));
+        setIsFave(user.favorites.includes(bridge._id));
     }, []);
-
-    const handleFavoritePressed = () => {
+    const handleFavoritePressed = async () => {
+        let updatedFavorites = [...user.favorites];
+        if (isFave) {
+            const bridgeIndex = updatedFavorites.indexOf(bridge._id);
+            if (bridgeIndex !== -1) {
+                updatedFavorites.splice(bridgeIndex, 1);
+            }
+        } else {
+            updatedFavorites.push(bridge._id);
+        }
         setIsFave(!isFave);
+        const response = await fetch(`${URL}/api/users/update/${user.id}`, {
+            method: 'PATCH',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                favorites: updatedFavorites
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
+        await refetch();
     }
     return (
         <View style={[styles.container, styles.bridge, styles.border]}>
